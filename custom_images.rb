86b58089c8291a843771b7df46d8cf3c90949b1c
747 <747.neutron+ghub@gmail.com>
2020-10-02 11:22:44 +0900

    selector images implemented

--- custom_images.rb
+++ custom_images.rb
@@ -4,12 +4,12 @@ w = 128
 h = 128
 cx = w / 2
 cy = h / 2
-rw = w * 0.9
-rh = h * 0.9
-rx = cx - rw / 2
-ry = cy - rh / 2
+rw = w
+rh = h
+rx = 0
+ry = 0
 rr = rw / 16
-diff = h * (1.0/3 - 1.0/20)
+diff = h * 1.0/3
 c = "#321760"
 
 set = {
@@ -27,20 +27,23 @@ set.each do |n, ent|
     context = Cairo::Context.new surface
     context.set_source_color c
 
-    context.stroke { context.rounded_rectangle rx, ry, rw, rh, rr }
+    context.fill { context.rounded_rectangle rx, ry, rw, rh, rr }
 
     context.select_font_face 'Aileron', nil, Cairo::FontWeight::BOLD
-    context.set_font_size 40
+    context.set_source_color "#FFFFFF"
+    context.set_font_size 48
     unless nx
       ext = context.text_extents n.upcase
       nx = cx - ext.width / 2 - ext.x_bearing
-      ny = h * (1.0/3 + 1.0/40) - ext.height / 2 - ext.y_bearing
+      ny = h * 1.0/3 - ext.height / 2 - ext.y_bearing
     end
     context.move_to nx, ny
     context.show_text n.upcase
     is = n == "tag" ? i == 0x20 ? "\u2423" : i == 0x7F ? "END" : i.chr : i.to_s
+    box = is == "\u2423"
+    context.select_font_face 'DejaVuSans', nil, Cairo::FontWeight::BOLD if box
     ix = context.text_extents is
-    context.move_to cx - ix.width / 2 - ix.x_bearing, ny + diff
+    context.move_to cx - ix.width / 2 - ix.x_bearing, ny + diff - (box ? h / 12.0 : 0)
     context.show_text is
 
     context.show_page
@@ -52,10 +55,11 @@ surface = Cairo::SVGSurface.new "public/images/selectors/zwj.svg", w, h
 context = Cairo::Context.new surface
 context.set_source_color c
 
-context.stroke { context.rounded_rectangle rx, ry, rw, rh, rr }
+context.fill { context.rounded_rectangle rx, ry, rw, rh, rr }
 
 context.select_font_face 'Aileron', nil, Cairo::FontWeight::BOLD
-context.set_font_size 40
+context.set_source_color "#FFFFFF"
+context.set_font_size 48
 ext = context.text_extents "ZWJ"
 context.move_to cx - ext.width / 2 - ext.x_bearing, cy - ext.height / 2 - ext.y_bearing
 context.show_text "ZWJ"
